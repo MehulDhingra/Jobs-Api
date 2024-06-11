eeae02dd28312437e7a86d165dff194266a426f1
MehulDhingra <dhingramehul24@gmail.com>
2024-06-11 15:36:23 +0530

    toString changed

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI.toString());
+    await connectDB(process.env.MONGO_URI);
     console.log("connected to DB");
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
